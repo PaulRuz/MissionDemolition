bdef5e18c3275b624b59e69e6114d87d67d7880c
Pavel Ruzanov <ruzanov@yahoo.com>
2019-12-11 11:17:14 +0300

    Initial commit

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
